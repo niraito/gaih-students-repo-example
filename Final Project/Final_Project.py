e2761481fc426aaa4a3dbcda2fcd5e80f1f33611
niraito <79105838+niraito@users.noreply.github.com>
2021-02-21 23:25:40 +0300

    Update Final_Project.py
    add something like final project

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -1,3 +1,51 @@
-import pandas as pd
+'''Recipe Application
+Enter 3 recipes.
+Create a separate class for each recipe.
+Identify the products used in this recipe using the init() method.
+Write a function about how long these products should be used later.
 
-data = pd.read_csv("project.csv")
+Use inheritance.
+E.g. cooking function; it will be common to all, so inherit it.'''
+
+class Kitchen():
+    def __init__(self, ingredient, mix_time, heat_time):
+        self.ingredient = ingredient
+        self.mix_time = mix_time
+        self.heat_time = heat_time
+    
+    def add(self, ingredient):
+        if ingredient:
+            print(f"{self.ingredient}: ekliyoruz...")
+        else:
+            print("Biraz ekliyoruz...")
+            
+    def mix(self, mix_time):
+        if mix_time:
+            print(f"{self.mix_time}: dakika karıştırıyoruz...")
+        else:
+            print("Biraz karıştırıyoruz...")
+    
+    def heat(self, heat_time):
+        if heat_time:
+            print(f"{self.heat_time}: dakika ısıtıyoruz...")
+        else:
+            print("Biraz ısıtıyoruz...")
+
+#class Ingredient():
+#    def __init__(self, cut)
+
+class Menemen(Kitchen):
+    def __init__(self, ingredient, mix_time, heat_time):
+        super().__init__(ingredient, mix_time, heat_time)
+
+class Pilav(Kitchen):
+    def __init__(self, ingredient, mix_time, heat_time):
+        super().__init__(ingredient, mix_time, heat_time)
+
+class Puding(Kitchen):
+    def __init__(self, ingredient, mix_time, heat_time):
+        super().__init__(ingredient, mix_time, heat_time)
+
+print(Menemen("Yumurta", 3, 15))
+print(Pilav("Pirinç", 2, 30))
+print(Puding("Puding tozu", 20, 30))
